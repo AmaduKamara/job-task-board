e9d3af7d10fb1df7b427a25e1eb521cc0ee390a8
Amkam <amaduskamara@gmail.com>
2023-07-27 06:37:08 +0000

    Initialize react app and tailwind config

---
+++ README.md
@@ -0,0 +1,8 @@
+# Job and Tasks Board App
+
+> Job Listing and task board app with authentication basics for adding and managing job posting
+
+## Built With
+
+- React
+- TailwindCSS
---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-gray-900 h-screen w-full">
+      <h1 className="text-center text-slate-100">Job Listing</h1>
+    </div>
+  );
+}
+
+export default App;
