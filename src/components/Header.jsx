b25a476c0f25b4e46bb98f836a84d717e0d3caf6
Amkam <amaduskamara@gmail.com>
2023-07-27 07:32:45 +0000

    Add routes links in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="py-6 border-b border-slate-100">
+      <div className="container mx-auto px-16 flex items-center justify-between">
+        <h1>
+          <Link to="/" className="text-white">
+            JobTasks Bard
+          </Link>
+        </h1>
+
+        <nav>
+          <ul className="flex space-x-8">
+            <li>
+              <Link to="/" className="text-gray-100 text-sm">
+                Job Listings
+              </Link>
+            </li>
+            <li>
+              <Link to="/tasks" className="text-gray-100 text-sm">
+                Task Board
+              </Link>
+            </li>
+            <li>
+              <Link to="/login" className="text-gray-100 text-sm">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
