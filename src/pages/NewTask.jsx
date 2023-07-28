ffcf9425f61dfb207bf8b5c6864c141f908623f8
Amkam <amaduskamara@gmail.com>
2023-07-28 16:33:29 +0000

    Update NewTask.jsx

--- src/pages/NewTask.jsx
+++ src/pages/NewTask.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NewTask = () => {
   return (
-    <div>
+    <div className="py-32">
       <h1 className="text-center text-slate-100">New Task</h1>
     </div>
   );
