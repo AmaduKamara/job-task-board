1f07b91a2adf44e5383ee0c1c74e40185432ad76
Amkam <amaduskamara@gmail.com>
2023-07-29 18:13:14 +0000

    Update Jobs.jsx

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,17 +1,35 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Job from "../components/Job";
+import { useEffect, useState } from "react";
 
 const Jobs = () => {
+  const [user, setUser] = useState();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getUser = async () => {
+      const userData = await JSON.parse(localStorage.getItem("user"));
+      setUser(userData);
+    };
+    getUser();
+  }, [navigate]);
+
+  const addListing = () => {
+    if (user) navigate("/create-listing");
+  };
+
   return (
     <div className="py-32 h-full">
       <div className="flex justify-between items-center">
         <h1 className="text-slate-200 text-lg md:text-3xl">Job Listings</h1>
-        <Link
-          to="/create-listing"
+        <button
+          disabled={!user}
           className="border py-2 px-5 border-gray-700 mr-2 rounded-md text-slate-200 text-sm"
+          onClick={addListing}
         >
-          Create Listing
-        </Link>
+          {user ? "Create Listing" : "Login to create listing"}
+        </button>
       </div>
       <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-8">
         <Job />
