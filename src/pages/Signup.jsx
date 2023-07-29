db81c0f69152468eb76e92cc7ecc10899a1a6868
Amkam <amaduskamara@gmail.com>
2023-07-29 15:57:59 +0000

    Add signup function and store user to localstorage

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,41 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!email || !password || !confirmPassword) {
+      setError("Please fill in all fields");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    const newUser = {
+      email,
+      password
+    };
+
+    localStorage.setItem("user", JSON.stringify(newUser));
+
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+
+    navigate("/login");
   };
+
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="w-1/3 border border-gray-700 rounded-md p-6 text-slate-400">
@@ -19,6 +51,8 @@ const Signup = () => {
             type="email"
             id="email"
             className="bg-transparent border border-gray-600 rounded-md py-2 px-4 w-full mb-5"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <label
             className="block mb-2 text-slate-300 text-sm"
@@ -30,6 +64,8 @@ const Signup = () => {
             type="password"
             id="password"
             className="bg-transparent border border-gray-600 rounded-md py-2 px-4 w-full mb-5"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <label
             className="block mb-2 text-slate-300 text-sm"
@@ -41,7 +77,10 @@ const Signup = () => {
             type="password"
             id="confirmPassword"
             className="bg-transparent border border-gray-600 rounded-md py-2 px-4 w-full mb-5"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
           />
+          {error && <p className="py-2 mb-3 text-sm text-red-300">{error}</p>}
           <div className="flex justify-end items-center">
             <Link to="/" className="px-5 text-slate-200 text-sm">
               Cancel
@@ -53,8 +92,18 @@ const Signup = () => {
               Login
             </Link>
             <button
-              disabled
-              className="bg-gray-700 border py-2 px-5 border-gray-700 rounded-md text-sm"
+              disabled={
+                (!email && !password && !confirmPassword) ||
+                password !== confirmPassword
+              }
+              className={`${
+                !email ||
+                !password ||
+                !confirmPassword ||
+                password !== confirmPassword
+                  ? "bg-gray-700  border py-2 px-5 border-gray-700 rounded-md text-sm text-slate-600"
+                  : "bg-slate-100 text-slate-800 border py-2 px-5 border-gray-700 rounded-md text-sm"
+              }`}
             >
               Sign Up
             </button>
