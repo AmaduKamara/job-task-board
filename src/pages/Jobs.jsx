0d5613dba3883aed34cf58e88fb2927c88244ebb
Amkam <amaduskamara@gmail.com>
2023-07-30 11:53:10 +0000

    Update Jobs.jsx

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 const Jobs = () => {
   const [user, setUser] = useState();
+  const [listings, setListings] = useState([]);
 
   const navigate = useNavigate();
 
@@ -15,12 +16,21 @@ const Jobs = () => {
     getUser();
   }, [navigate]);
 
+  useEffect(() => {
+    const fetchListings = async () => {
+      const listings = await JSON.parse(localStorage.getItem("listings"));
+      setListings(listings);
+    };
+
+    fetchListings();
+  }, []);
+
   const addListing = () => {
     if (user) navigate("/create-listing");
   };
 
   return (
-    <div className="py-32 h-full">
+    <div className="py-32 h-screen">
       <div className="flex justify-between items-center">
         <h1 className="text-slate-200 text-lg md:text-3xl">Job Listings</h1>
         <button
@@ -32,21 +42,15 @@ const Jobs = () => {
         </button>
       </div>
       <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-8">
-        <Job />
-        <Job />
-        <Job />
-        <Job />
-        <Job />
-        <Job />
-        <Job />
-        <Job />
-        <Job />
-        <Job />
-        <Job />
-        <Job />
-        <Job />
-        <Job />
-        <Job />
+        {!listings && (
+          <div className="w-full">
+            <h2 className="text-lg md:text-2xl text-slate-200">
+              Sorry, no listings found! 👍🏽
+            </h2>
+          </div>
+        )}
+        {listings &&
+          listings.map((listing) => <Job key={listing.id} listing={listing} />)}
       </div>
     </div>
   );
